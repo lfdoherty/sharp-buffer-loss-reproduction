f3e9efd2da69d96e17ce72b9e6682753cf64acd2
lfdoherty <lfdoherty@gmail.com>
2017-09-15 18:54:02 +0800

    added msg

--- repro.js
+++ repro.js
@@ -19,7 +19,7 @@ const rs = readAllFilesInTar('s_covers_0000_00.tar', (entry, doneCb) => {
 			.raw()
 			.toBuffer((err, data, info) => {
 				if(err){
-					//if(buf2[0] !== buf2[1]){}//DELETE/COMMENT this line to trigger error
+					if(buf2[0] !== buf2[1]){}//DELETE/COMMENT this line to trigger error
 					throw err;
 				}
 				++okCount;
